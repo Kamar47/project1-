3e18b81247e6a72b64bc67ff4defe701eaf28603
mhmod-khalaile <mj7vw2ctrb@privaterelay.appleid.com>
2026-06-04 14:15:47 +0300

    Delete G11_Assignment3/src directory

--- G11_Assignment3/src/client/ClientMessageHandler.java
+++
@@ -1,8 +0,0 @@
-package client;
-
-import common.ClientServerMessage;
-
-public interface ClientMessageHandler {
-    void handleMessage(ClientServerMessage msg);
-    void onDisconnected(String reason);
-}
